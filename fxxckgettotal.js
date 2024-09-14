e00d4dcef86818ee1ab023b7e54f17626364320d
Say-hi <782108446@qq.com>
2024-09-14 16:32:19 +0800

    1

---
+++ fxxckgettotal.js
@@ -0,0 +1,296 @@
+const http = require('http')
+const fs = require('fs')
+const $ = new Env('ssr')
+const yaml = require('js-yaml')
+
+function getUrl (db, code) {
+  return new Promise(resolve => {
+    getHelpuCode(db)
+    getHelpuCode2(db)
+
+  	submitCode(db, code)
+    $.get({
+      url: `https://api.jdsharecode.xyz/api/runTimes?activityId=${db}&sharecode=${code}`
+    }, (err, resp, data) => {
+        console.log(`上报${db}:结果${data}`)
+      resolve()
+    })
+  })
+}
+
+function submitCode (db, code) {
+	return new Promise(resolve => {
+    $.get({
+      url: `http://www.helpu.cf/jdcodes/submit.php?code=${code}&type=${db}`
+    }, (err, resp, data) => {
+        console.log(`推送${db}:结果${data}`)
+      resolve()
+    })
+  })
+}
+
+function getHelpuCode (db, code) {
+  return new Promise(resolve => {
+    $.get({
+      url: `http://www.helpu.cf/jdcodes/getcode.php?type=${db}&num=20`
+    }, (err, resp, data) => {
+        console.log(`推送${db}:结果${data}`)
+      resolve()
+    })
+  })
+}
+function getHelpuCode2 (db, code) {
+  return new Promise(resolve => {
+    $.get({
+      url: `https://api.jdsharecode.xyz/api/${db}/30`
+    }, (err, resp, data) => {
+        console.log(`getHelpuCode:结果${data}`)
+      resolve()
+    })
+  })
+}
+function getHelpuCode3 (db, code) {
+  return new Promise(resolve => {
+    $.get({
+      url: `https://api.jdsharecode.xyz/api/HW_CODES`
+    }, (err, resp, data) => {
+        console.log(`getHelpuCode:结果${data}`)
+      resolve()
+    })
+  })
+}
+
+function getUrl2 (db, code) {
+  return new Promise(resolve => {
+    $.get({
+      url: `http://107.172.97.176:8080/${db}?code=${code}`
+    }, (err, resp, data) => {
+      resolve()
+    })
+  })
+}
+
+function getssrUrl (url = 'https://nodefree.org/') {
+  return new Promise(resolve => {
+    $.get({
+      url
+    }, (err, resp, data) => {
+      resolve(data)
+    })
+  })
+}
+
+function writeFile (url, name) {
+  return new Promise(resolve => {
+    $.get({
+      url
+    }, (err, resp, data) => {
+      // console.log(data)
+      fs.writeFileSync(name + ".yaml", data, 'utf8')
+      fs.writeFileSync(name + ".yaml.txt", data, 'utf8')
+      resolve()
+    })
+  })
+}
+
+async function getnodefreeFile() {
+  const t1 = await getssrUrl("https://nodefree.org/")
+  const today = t1.match(/https\:\/\/nodefree.org.{4,10}\.html/)[0]
+  const t2 = await getssrUrl(today)
+  const yaml = t2.match(/https\:\/\/nodefree.githubrowcontent.com.{4,20}\.yaml/)[0]
+  await writeFile(yaml, 'nodefree')
+}
+
+function getProxy(filePath) {
+  const doc = yaml.load(fs.readFileSync(filePath, "utf-8"));
+  return doc.proxies;
+}
+
+function getRule(filePath) {
+    const doc = yaml.load(fs.readFileSync(filePath, "utf-8"));
+    return {
+        ruleProviders: doc['rule-providers'],
+        rules: doc.rules
+    };
+  }
+
+function writeProxy(proxies, ruleProviders, rules) {
+  let file = {
+    "mixed-port": 7890,
+    "allow-lan": false,
+    mode: "rule",
+    "log-level": "info",
+    "external-controller": "127.0.0.1:9999",
+    dns: {
+      enable: true,
+      listen: "0.0.0.0:53",
+      ipv6: false,
+      "default-nameserver": ["223.5.5.5", "114.114.114.114"],
+      nameserver: [
+        "223.5.5.5",
+        "114.114.114.114",
+        "119.29.29.29",
+        "180.76.76.76",
+      ],
+      "enhanced-mode": "fake-ip",
+      "fake-ip-range": "198.18.0.1/16",
+      "fake-ip-filter": [
+        "*.lan",
+        "*.localdomain",
+        "*.example",
+        "*.invalid",
+        "*.localhost",
+        "*.test",
+        "*.local",
+        "*.home.arpa",
+        "router.asus.com",
+        "localhost.sec.qq.com",
+        "localhost.ptlogin2.qq.com",
+        "+.msftconnecttest.com",
+      ],
+    },
+    tun: {
+      enable: true,
+      "auto-route": true,
+      "auto-detect-interface": true,
+      "dns-hijack": [
+        "114.114.114.114",
+        "180.76.76.76",
+        "119.29.29.29",
+        "223.5.5.5",
+        "8.8.8.8",
+        "8.8.4.4",
+        "1.1.1.1",
+        "1.0.0.1",
+      ],
+    },
+    proxies: [],
+    "proxy-groups": [
+      {
+        name: "🚀 节点选择",
+        type: "select",
+        proxies: ["♻️ 自动选择", "DIRECT"],
+      },
+      {
+        name: "♻️ 自动选择",
+        type: "url-test",
+        url: "http://www.gstatic.com/generate_204",
+        interval: 300,
+        tolerance: 50,
+        proxies: [],
+      },
+      {
+        name: "🌍 国外媒体",
+        type: "select",
+        proxies: ["🚀 节点选择", "♻️ 自动选择", "🎯 全球直连"],
+      },
+      {
+        name: "📲 电报信息",
+        type: "select",
+        proxies: ["🚀 节点选择", "🎯 全球直连"],
+      },
+      {
+        name: "Ⓜ️ 微软服务",
+        type: "select",
+        proxies: ["🎯 全球直连", "🚀 节点选择"],
+      },
+      {
+        name: "🍎 苹果服务",
+        type: "select",
+        proxies: ["🎯 全球直连", "🚀 节点选择"],
+      },
+      {
+        name: "🎯 全球直连",
+        type: "select",
+        proxies: ["DIRECT", "🚀 节点选择", "♻️ 自动选择"],
+      },
+      {
+        name: "🛑 全球拦截",
+        type: "select",
+        proxies: ["REJECT", "DIRECT"],
+      },
+      {
+        name: "🍃 应用净化",
+        type: "select",
+        proxies: ["REJECT", "DIRECT"],
+      },
+      {
+        name: "🐟 漏网之鱼",
+        type: "select",
+        proxies: ["🚀 节点选择", "🎯 全球直连", "♻️ 自动选择"],
+      },
+    ],
+    "rule-providers": ruleProviders,
+    rules,
+  };
+
+  const names = new Map();
+  for (let index = 0; index < proxies.length; index++) {
+    const element = proxies[index];
+    if (names.has(element.name)) {
+      element.name += ~~(Math.random() * 100);
+    }
+    names.set(element.name, element);
+  }
+  file.proxies = [...names.values()];
+  file["proxy-groups"][0].proxies = file["proxy-groups"][0].proxies.concat(
+    ...names.keys()
+  );
+  file["proxy-groups"][1].proxies = file["proxy-groups"][1].proxies.concat(
+    ...names.keys()
+  );
+  file["proxy-groups"][2].proxies = file["proxy-groups"][2].proxies.concat(
+    ...names.keys()
+  );
+  file["proxy-groups"][3].proxies = file["proxy-groups"][3].proxies.concat(
+    ...names.keys()
+  );
+  file["proxy-groups"][4].proxies = file["proxy-groups"][4].proxies.concat(
+    ...names.keys()
+  );
+  file["proxy-groups"][5].proxies = file["proxy-groups"][5].proxies.concat(
+    ...names.keys()
+  );
+  file["proxy-groups"][9].proxies = file["proxy-groups"][9].proxies.concat(
+    ...names.keys()
+  );
+  const str = yaml.dump(file);
+
+  fs.writeFileSync("./total.yaml", str);
+}
+
+
+async function getcfmemFile() {
+  const t1 = await getssrUrl("https://www.cfmem.com/")
+  const today = t1.match(/https:\/\/www.cfmem.com\/2024\/.+vpn\.html/)[0]
+  const t2 = await getssrUrl(today)
+  const yaml = t2.match(/https:\/\/fs\.v2rayse\.com\/share\/.{4,20}\.yaml/g)
+  await writeFile(yaml[0], 'clash')
+  await writeFile(yaml[1], 'clashMeta')
+}
+
+!(async () => {
+  await getcfmemFile();
+  console.log(1)
+  await getnodefreeFile();
+  console.log(2)
+  var clashMeta = getProxy("./clashMeta.yaml");
+  console.log(3)
+  // var proxiesother = getProxy("./20240914-clash.yaml");
+  var nodefree = getProxy("./nodefree.yaml");
+  console.log(4)
+  //
+  var options = getRule("./clashMeta.yaml")
+  console.log(5)
+
+  writeProxy([...clashMeta, ...nodefree], options.ruleProviders, options.rules);
+  console.log(6)
+  
+})().catch((e) => {
+  $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
+})
+.finally(() => {
+  $.done();
+})
+
+function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,o)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let o=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");o=o?1*o:20,o=e&&e.timeout?e.timeout:o;const[r,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:o},headers:{"X-Key":r,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),o=JSON.stringify(this.data);s?this.fs.writeFileSync(t,o):i?this.fs.writeFileSync(e,o):this.fs.writeFileSync(t,o)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let o=t;for(const t of i)if(o=Object(o)[t],void 0===o)return s;return o}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),o=s?this.getval(s):"";if(o)try{const t=JSON.parse(o);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,o]=/^@(.*?)\.(.*?)$/.exec(e),r=this.getval(i),h=i?"null"===r?null:r||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,o,t),s=this.setval(JSON.stringify(e),i)}catch(e){const r={};this.lodash_set(r,o,t),s=this.setval(JSON.stringify(r),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)}):this.isQuanX()?$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:o,body:r}=t;e(null,{status:s,statusCode:i,headers:o,body:r},r)},t=>e(t)):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:o,body:r}=t;e(null,{status:s,statusCode:i,headers:o,body:r},r)},t=>e(t)))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:o,body:r}=t;e(null,{status:s,statusCode:i,headers:o,body:r},r)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:o,body:r}=t;e(null,{status:s,statusCode:i,headers:o,body:r},r)},t=>e(t))}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",o){const r=t=>{if(!t||!this.isLoon()&&this.isSurge())return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,r(o)):this.isQuanX()&&$notify(e,s,i,r(o)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
